6bd83cadd6cb3bc3f59f3d2233955cc4f5d1f117
AngryPapaHerny <102565063+AngryPapaHerny@users.noreply.github.com>
2023-07-24 18:49:59 +0900

    Update AISearch.py

--- AISearch.py
+++ AISearch.py
@@ -39,17 +39,19 @@ with st.form("form"):
         user_input = st.text_input("Prompt")
         submit = st.form_submit_button("Submit")
 
-if submit and user_input:
-    	with st.spinner("waiting..."):
-            response = Bard(token).get_answer(user_input)
 
-        try:
-            answers = [choice['content'][0] for choice in response['choices']]
-            num_tabs = min(len(answers), 3)
-            tab1, tab2, tab3 = st.tabs([f'answer{i}' for i in range(1, num_tabs + 1)])
+with st.spinner("waiting..."):
+        if submit and user_input:
+                response = Bard(token).get_answer(user_input)
+try:
+        answers = [choice['content'][0] for choice in response['choices']]
+        tab1, tab2, tab3 = st.tabs(['answer1', 'answer2','answer3'])
 
-            for i in range(num_tabs):
-                with globals()[f'tab{i+1}']:
-                    st.write(answers[i])
-        except (KeyError, IndexError):
+        with tab1:
+            st.write(answers[0])
+        with tab2:
+            st.write(answers[1])
+        with tab3:
+            st.write(answers[2])
+except (KeyError, IndexError):
             st.write("무엇을 도와드릴까요?")
