3ed18ee0e2f065b3985b2b9e77cd228261a31870
AngryPapaHerny <102565063+AngryPapaHerny@users.noreply.github.com>
2023-07-24 13:05:00 +0900

    Update and rename test.py to AISearch.py

---
+++ AISearch.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import bardapi 
+
+token = st.secrets["bard_api"]
+
+theme = st.sidebar.radio('테마',['default','dark'])
+option = st.sidebar.selectbox(
+    'Menu',
+     ('검색', '멀티미디어', '기타'))
+
+if option == '검색':
+    st.title("인공지능 검색 서비스")
+
+    with st.form("form"):
+        user_input = st.text_input("Prompt")
+        submit = st.form_submit_button("Submit")
+
+    response=''
+    with st.spinner("waiting..."):
+        if submit and user_input:
+            response = bardapi.core.Bard(token).get_answer(user_input)
+        # st.write(response['choices'])
+
+    answers=[]
+
+    try:
+        # for i,choice in enumerate(response['choices']):
+        for choice in response['choices']:
+            answers.append(choice['content'][0])
+
+        tab1, tab2, tab3 = st.tabs(['answer1', 'answer2','answer3'])
+
+        with tab1:
+            st.write(answers[0])
+        with tab2:
+            st.write(answers[1])
+        with tab3:
+            st.write(answers[2])
+    except:
+        st.write("무엇을 도와드릴까요?")
+
+elif option == '멀티미디어':
+    st.title("페이지 2")
+
+else : 
+    st.title('페이지 3')
